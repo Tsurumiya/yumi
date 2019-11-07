2ac7c0b2315a1a7ee55bbbaae502b476aad867d7
Tsurumiya <visitor96@gmail.com>
2019-11-07 19:52:02 +0900

    ステージ修正 & リセット機能改良 & 発射機構(工事中)

--- yumi/Assets/Nin/Scripts/PlayerController.cs
+++ yumi/Assets/Nin/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour {
 	
 	private List<Joycon> joycons;
 
-    // Values made available via Unity
+    //Joycon
     public float[] stick;
     public Vector3 gyro;
     public Vector3 accel;
@@ -14,10 +14,22 @@ public class PlayerController : MonoBehaviour {
     public Quaternion orientation;
     public Quaternion orientation_original;
 
+    //Arduino
+    public SerialController serialController;
+    private float shootForce;
+    private bool isShooting = false;
+
     //弓矢関連
-    public GameObject throwObject;
-    public GameObject itemSpawnPoint;
+    [SerializeField]
+    private GameObject shootObject;
+    [SerializeField]
+    private GameObject itemSpawnPoint;
     private GameObject cloneObject;
+
+    private Color pointerColor;
+    [SerializeField]
+    private float fadeSpeed;
+    private bool isYPressed = false;
 
     void Start ()
     {
@@ -28,14 +40,21 @@ public class PlayerController : MonoBehaviour {
 		if (joycons.Count < jc_ind+1){
 			Destroy(gameObject);
 		}
-	}
+
+        serialController = GameObject.Find("SerialController").GetComponent<SerialController>();
+
+        pointerColor = itemSpawnPoint.GetComponent<MeshRenderer>().material.color;
+    }
 
     // Update is called once per frame
     void Update () {
+        //Joycon//
 		// make sure the Joycon only gets checked if attached
 		if (joycons.Count > 0)
         {
 			Joycon j = joycons [jc_ind];
+
+            /*
 			// GetButtonDown checks if a button has been pressed (not held)
             if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
             {
@@ -69,6 +88,7 @@ public class PlayerController : MonoBehaviour {
                 // (Useful for dynamically changing rumble values.)
 				// Then call SetRumble(0,0,0) when you want to turn it off.
 			}
+            */
 
             stick = j.GetStick();
 
@@ -78,21 +98,61 @@ public class PlayerController : MonoBehaviour {
             // Accel values:  x, y, z axis values (in Gs)
             accel = j.GetAccel();
 
-            //orientation = j.GetVector();
             orientation = Quaternion.identity * 
                 Quaternion.AngleAxis(-j.GetVector().eulerAngles.x, Vector3.right) * 
                 Quaternion.AngleAxis(j.GetVector().eulerAngles.y, Vector3.forward) * 
                 Quaternion.AngleAxis(-j.GetVector().eulerAngles.z, Vector3.up);
+
             if (j.GetButtonUp(Joycon.Button.DPAD_LEFT))     //角度修正（なぜか2回押さなければ効かない）
-            {                
+            {
+                isYPressed = true;
                 gameObject.transform.rotation = Quaternion.identity;
                 orientation_original = orientation;
                 j.Recenter();
+                StartCoroutine(FadeOut());
             }
             else
             {
                 gameObject.transform.rotation = orientation * Quaternion.Inverse(orientation_original);
             }
+
+            //発射
+            if (isShooting == true) 
+            {
+                isShooting = false;
+                cloneObject = Instantiate(shootObject, itemSpawnPoint.transform.position, itemSpawnPoint.transform.rotation);
+                cloneObject.SetActive(true);
+                cloneObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, shootForce);
+            }
+        }
+
+        //Arduino//
+        string message = serialController.ReadSerialMessage();
+        if (message != null)
+        {
+            // Check if the message is plain data or a connect/disconnect event.
+            if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_CONNECTED))
+                Debug.Log("Connection established");
+            else if (ReferenceEquals(message, SerialController.SERIAL_DEVICE_DISCONNECTED))
+                Debug.Log("Connection attempt failed or disconnection detected");
+            else
+            {
+                Debug.Log("Message arrived: " + message);
+                shootForce = float.Parse(message);
+            }
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        pointerColor.a = 255;
+        yield return new WaitForSeconds(0.5f);
+        isYPressed = false;
+        while (pointerColor.a > 0)
+        {
+            pointerColor.a -= Time.deltaTime * fadeSpeed;
+            if (isYPressed == true)
+                yield break;
         }
     }
 }
