ac8c07cc43bd1d3f9f1626d8d9ade8e06801dcab
Yoshida Masaki <>
2019-11-12 15:35:21 +0900

    title 調整

--- yumi/Assets/Yoshida/title/GameSystem.cs
+++ yumi/Assets/Yoshida/title/GameSystem.cs
@@ -36,40 +36,32 @@ public class GameSystem : MonoBehaviour
         {
             Joycon j = joycons[jc_ind];
             // GetButtonDown checks if a button has been pressed (not held)
+            if (j.GetButtonDown(Joycon.Button.SHOULDER_1))
+            {
+                SceneManager.LoadScene("Stage");
+            }
+            // GetButtonDown checks if a button has been release
             if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
             {
-                Debug.Log("Shoulder button 2 pressed");
-                // GetStick returns a 2-element vector with x/y joystick components
-                Debug.Log(string.Format("Stick x: {0:N} Stick y: {1:N}", j.GetStick()[0], j.GetStick()[1]));
-
-                // Joycon has no magnetometer, so it cannot accurately determine its yaw value. Joycon.Recenter allows the user to reset the yaw value.
-                j.Recenter();
-            }
-            // GetButtonDown checks if a button has been released
-            if (j.GetButtonUp(Joycon.Button.SHOULDER_2))
-            {
-                Debug.Log("Shoulder button 2 released");
-            }
-            // GetButtonDown checks if a button is currently down (pressed or held)
-            if (j.GetButton(Joycon.Button.SHOULDER_2))
-            {
-                Debug.Log("Shoulder button 2 held");
+                SceneManager.LoadScene("Stage");
             }
 
             if (j.GetButtonDown(Joycon.Button.DPAD_DOWN))
             {
-                Debug.Log("Rumble");
-
-                // Rumble for 200 milliseconds, with low frequency rumble at 160 Hz and high frequency rumble at 320 Hz. For more information check:
-                // https://github.com/dekuNukem/Nintendo_Switch_Reverse_Engineering/blob/master/rumble_data_table.md
-
-                j.SetRumble(160, 320, 0.6f, 200);
-
-                // The last argument (time) in SetRumble is optional. Call it with three arguments to turn it on without telling it when to turn off.
-                // (Useful for dynamically changing rumble values.)
-                // Then call SetRumble(0,0,0) when you want to turn it off.
+                SceneManager.LoadScene("Stage");
             }
-
+            if (j.GetButton(Joycon.Button.DPAD_RIGHT))
+            {
+                SceneManager.LoadScene("Stage");   //change the name of scene
+            }
+            if (j.GetButton(Joycon.Button.DPAD_UP))
+            {
+                SceneManager.LoadScene("Stage");   //change the name of scene
+            }
+            if (j.GetButton(Joycon.Button.DPAD_LEFT))
+            {
+                SceneManager.LoadScene("Stage");   //change the name of scene
+            }
             stick = j.GetStick();
 
             // Gyro values: x, y, z axis values (in radians per second)
@@ -79,10 +71,6 @@ public class GameSystem : MonoBehaviour
             accel = j.GetAccel();
 
             orientation = j.GetVector();
-            if (j.GetButton(Joycon.Button.DPAD_RIGHT))
-            {
-                SceneManager.LoadScene("Scene1");   //change the name of scene
-            }
             
             gameObject.transform.rotation = orientation;
         }
