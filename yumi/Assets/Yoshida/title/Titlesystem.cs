987656dcfb11c60d135a1278de8070a85a7dd76e
Yoshida Masaki <>
2019-11-13 16:14:00 +0900

    矢　調整

---
+++ yumi/Assets/Yoshida/title/Titlesystem.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Titlesystem : MonoBehaviour
+{
+
+    private List<Joycon> joycons;
+
+    // Values made available via Unity
+    public float[] stick;
+    public Vector3 gyro;
+    public Vector3 accel;
+    public int jc_ind = 0;
+    public Quaternion orientation;
+
+    void Start()
+    {
+        gyro = new Vector3(0, 0, 0);
+        accel = new Vector3(0, 0, 0);
+        // get the public Joycon array attached to the JoyconManager in scene
+        joycons = JoyconManager.Instance.j;
+        if (joycons.Count < jc_ind + 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // make sure the Joycon only gets checked if attached
+        if (joycons.Count > 0)
+        {
+            Joycon j = joycons[jc_ind];
+            // GetButtonDown checks if a button has been pressed (not held)
+            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+            {
+				SceneManager.LoadScene("Scene1");
+			}
+            
+            
+            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+            {
+				SceneManager.LoadScene("Scene1");
+			}
+
+            if (j.GetButtonDown(Joycon.Button.DPAD_DOWN))
+            {
+				SceneManager.LoadScene("Scene1");
+			}
+
+         
+        }
+    }
+}
