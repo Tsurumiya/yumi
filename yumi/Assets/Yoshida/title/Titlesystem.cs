2bc714c0597aa0d437aa978517f79bb868fd9068
Yoshida Masaki <>
2019-11-19 02:11:03 +0900

    軌跡　追加

--- yumi/Assets/Yoshida/title/Titlesystem.cs
+++ yumi/Assets/Yoshida/title/Titlesystem.cs
@@ -6,52 +6,78 @@ using UnityEngine.SceneManagement;
 public class Titlesystem : MonoBehaviour
 {
 
-    private List<Joycon> joycons;
+	private List<Joycon> joycons;
 
-    // Values made available via Unity
-    public float[] stick;
-    public Vector3 gyro;
-    public Vector3 accel;
-    public int jc_ind = 0;
-    public Quaternion orientation;
+	// Values made available via Unity
+	public float[] stick;
+	public Vector3 gyro;
+	public Vector3 accel;
+	public int jc_ind = 0;
+	public Quaternion orientation;
 
-    void Start()
-    {
-        gyro = new Vector3(0, 0, 0);
-        accel = new Vector3(0, 0, 0);
-        // get the public Joycon array attached to the JoyconManager in scene
-        joycons = JoyconManager.Instance.j;
-        if (joycons.Count < jc_ind + 1)
-        {
-            Destroy(gameObject);
-        }
-    }
+	void Start()
+	{
+		gyro = new Vector3(0, 0, 0);
+		accel = new Vector3(0, 0, 0);
+		// get the public Joycon array attached to the JoyconManager in scene
+		joycons = JoyconManager.Instance.j;
+		if (joycons.Count < jc_ind + 1)
+		{
+			Destroy(gameObject);
+		}
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        // make sure the Joycon only gets checked if attached
-        if (joycons.Count > 0)
-        {
-            Joycon j = joycons[jc_ind];
-            // GetButtonDown checks if a button has been pressed (not held)
-            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
-            {
+	// Update is called once per frame
+	void Update()
+	{
+		// make sure the Joycon only gets checked if attached
+		if (joycons.Count > 0)
+		{
+			Joycon j = joycons[jc_ind];
+			// GetButtonDown checks if a button has been pressed (not held)
+			if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+			{
+				Debug.Log("Shoulder button 2 pressed");
+				// GetStick returns a 2-element vector with x/y joystick components
+				Debug.Log(string.Format("Stick x: {0:N} Stick y: {1:N}", j.GetStick()[0], j.GetStick()[1]));
+
+				// Joycon has no magnetometer, so it cannot accurately determine its yaw value. Joycon.Recenter allows the user to reset the yaw value.
+				j.Recenter();
+			}
+			// GetButtonDown checks if a button has been released
+			if (j.GetButtonUp(Joycon.Button.SHOULDER_2))
+			{
+				Debug.Log("Shoulder button 2 released");
+			}
+			// GetButtonDown checks if a button is currently down (pressed or held)
+			if (j.GetButton(Joycon.Button.SHOULDER_2))
+			{
+				Debug.Log("Shoulder button 2 held");
+			}
+
+			if (j.GetButtonDown(Joycon.Button.DPAD_DOWN))
+			{
 				SceneManager.LoadScene("Scene1");
 			}
-            
-            
-            if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
-            {
-				SceneManager.LoadScene("Scene1");
-			}
 
-            if (j.GetButtonDown(Joycon.Button.DPAD_DOWN))
-            {
-				SceneManager.LoadScene("Scene1");
-			}
+			stick = j.GetStick();
 
-         
-        }
-    }
+			// Gyro values: x, y, z axis values (in radians per second)
+			gyro = j.GetGyro();
+
+			// Accel values:  x, y, z axis values (in Gs)
+			accel = j.GetAccel();
+
+			orientation = j.GetVector();
+			if (j.GetButton(Joycon.Button.DPAD_UP))
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.red;
+			}
+			else
+			{
+				gameObject.GetComponent<Renderer>().material.color = Color.blue;
+			}
+			gameObject.transform.rotation = orientation;
+		}
+	}
 }
